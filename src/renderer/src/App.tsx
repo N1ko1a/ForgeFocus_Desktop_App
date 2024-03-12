f5b545ac47f575de2546a70208c5d16edec5686d
N1ko1a <ivanovicn98@gmail.com>
2024-03-12 15:06:35 +0100

    Funkcionalnost prvog tajmera uglavnom zavrsena

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
   return (
     <div className="bg-[url('/home/nikola/Nikola/github/Productivity_Desktop_App/src/renderer/src/assets/pxfuel.jpg')] w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center">
       <div className=" text-white w-screen h-1/5">
-        <Timer initialHours={1} initialMinutes={30} initialSeconds={10} />
+        <Timer />
       </div>
       <div className="flex justify-center w-screen h-4/5 mb-10 mr-0 lg:mr-20">
         <Kalendar />
--- src/renderer/src/components/Timer.tsx
+++ src/renderer/src/components/Timer.tsx
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const Timer = ({ initialHours, initialMinutes, initialSeconds }) => {
-  const [hours, setHours] = useState(initialHours || 0)
-  const [minutes, setMinutes] = useState(initialMinutes || 0)
-  const [seconds, setSeconds] = useState(initialSeconds || 0)
+const Timer = () => {
+  const storedHours = parseInt(window.localStorage.getItem('Hours'))
+  const storedMinutes = parseInt(window.localStorage.getItem('Minutes'))
+  const storedSeconds = parseInt(window.localStorage.getItem('Seconds'))
+
+  const [hours, setHours] = useState(!isNaN(storedHours) ? storedHours : 0)
+  const [minutes, setMinutes] = useState(!isNaN(storedMinutes) ? storedMinutes : 45)
+  const [seconds, setSeconds] = useState(!isNaN(storedSeconds) ? storedSeconds : 0)
   const [isActive, setIsActive] = useState(false)
+  const [hourIsClicked, setHourIsClicked] = useState(false)
+  const [minutesIsClicked, setMinutesIsClicked] = useState(false)
+  const [secondsIsClicked, setSecondsIsClicked] = useState(false)
+  const hourRef = useRef(null)
+  const minutesRef = useRef(null)
+  const secondsRef = useRef(null)
 
   useEffect(() => {
     let intervalId
@@ -40,13 +50,124 @@ const Timer = ({ initialHours, initialMinutes, initialSeconds }) => {
     setSeconds(initialSeconds)
   }
 
-  const formatTime = (h, m, s) => {
-    return `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`
+  const handleHourClick = () => {
+    setHourIsClicked(true)
   }
 
+  const handleMinutesClick = () => {
+    setMinutesIsClicked(true)
+  }
+
+  const handleSecondsClick = () => {
+    setSecondsIsClicked(true)
+  }
+
+  const handleHoursUpdate = (event) => {
+    const newHours = event.target.value
+    window.localStorage.setItem('Hours', newHours)
+    setHours(newHours)
+  }
+
+  const handleMinutesUpdate = (event) => {
+    let newMinutes = event.target.value
+    newMinutes = newMinutes.slice(0, 2)
+    if (newMinutes >= 0 && newMinutes <= 59) {
+      window.localStorage.setItem('Minutes', newMinutes)
+      setMinutes(newMinutes)
+    }
+  }
+
+  const handleSecondsUpdate = (event) => {
+    let newSeconds = event.target.value
+    // Ograničavamo dužinu na dve cifre
+    newSeconds = newSeconds.slice(0, 2)
+    // Ako je unutar opsega 0-59, čuvamo vrednost i ažuriramo lokalno skladište
+    if (newSeconds >= 0 && newSeconds <= 59) {
+      window.localStorage.setItem('Seconds', newSeconds)
+      setSeconds(newSeconds)
+    }
+  }
+
+  const handleKeyDownUpdate = (event) => {
+    if (event.key === 'Enter') {
+      setHourIsClicked(false)
+      setMinutesIsClicked(false)
+      setSecondsIsClicked(false)
+    }
+  }
+
+  useEffect(() => {
+    if (!hourIsClicked) return
+    if (hourRef.current) {
+      hourRef.current.focus()
+    }
+  }, [hourIsClicked])
+
+  useEffect(() => {
+    if (!minutesIsClicked) return
+    if (minutesRef.current) {
+      minutesRef.current.focus()
+    }
+  }, [minutesIsClicked])
+
+  useEffect(() => {
+    if (!secondsIsClicked) return
+    if (secondsRef.current) {
+      secondsRef.current.focus()
+    }
+  }, [secondsIsClicked])
   return (
     <div>
-      <h1>Countdown Timer: {formatTime(hours, minutes, seconds)}</h1>
+      <div className="flex">
+        {hourIsClicked ? (
+          <input
+            ref={hourRef}
+            type="number"
+            placeholder={hours}
+            value={hours}
+            onChange={handleHoursUpdate}
+            onKeyDown={handleKeyDownUpdate}
+            onBlur={() => setHourIsClicked(false)}
+            className="w-full h-full bg-transparent rounded-lg text-white pl-4 outline-none"
+          />
+        ) : (
+          <h1 onClick={handleHourClick}>{hours < 10 ? '0' + hours : hours}</h1>
+        )}
+        <h1>:</h1>
+        {minutesIsClicked ? (
+          <input
+            ref={minutesRef}
+            type="number"
+            placeholder={minutes}
+            value={minutes}
+            onChange={handleMinutesUpdate}
+            onKeyDown={handleKeyDownUpdate}
+            onBlur={() => setMinutesIsClicked(false)}
+            className="w-full h-full bg-transparent rounded-lg text-white pl-4 outline-none"
+          />
+        ) : (
+          <h1 onClick={handleMinutesClick}>
+            {minutes < 10 && minutes.length < 2 ? '0' + minutes : minutes}
+          </h1>
+        )}
+        <h1>:</h1>
+        {secondsIsClicked ? (
+          <input
+            ref={secondsRef}
+            type="number"
+            placeholder={seconds}
+            value={seconds}
+            onChange={handleSecondsUpdate}
+            onKeyDown={handleKeyDownUpdate}
+            onBlur={() => setSecondsIsClicked(false)}
+            className="w-full h-full bg-transparent rounded-lg text-white pl-4 outline-none"
+          />
+        ) : (
+          <h1 onClick={handleSecondsClick}>
+            {seconds < 10 && seconds.length < 2 ? '0' + seconds : seconds}
+          </h1>
+        )}
+      </div>
       <button onClick={toggleTimer}>{isActive ? 'Pause' : 'Start'}</button>
       <button onClick={resetTimer}>Reset</button>
     </div>
